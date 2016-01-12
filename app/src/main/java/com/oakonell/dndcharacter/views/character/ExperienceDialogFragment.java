8faf9e81391207e0b441ebabf67de63ceecc0853
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-01-12 13:35:36 -0500

    Add Xp levels list to xp dialog

--- app/src/main/java/com/oakonell/dndcharacter/views/character/ExperienceDialogFragment.java
+++ app/src/main/java/com/oakonell/dndcharacter/views/character/ExperienceDialogFragment.java
@@ -1,6 +1,7 @@
 package com.oakonell.dndcharacter.views.character;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -11,6 +12,13 @@ import android.widget.TextView;
 
 import com.oakonell.dndcharacter.R;
 import com.oakonell.dndcharacter.model.character.Character;
+import com.oakonell.dndcharacter.views.BindableComponentViewHolder;
+import com.oakonell.dndcharacter.views.DividerItemDecoration;
+
+import org.solovyev.android.views.llm.LinearLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Rob on 1/7/2016.
@@ -26,6 +34,9 @@ public class ExperienceDialogFragment extends AbstractCharacterDialogFragment {
 
     private ViewGroup your_xp_group;
     private TextView your_xp;
+
+    private RecyclerView xp_levels;
+    private XpLevelAdapter xpAdapter;
 
     public static ExperienceDialogFragment create() {
         return new ExperienceDialogFragment();
@@ -49,7 +60,44 @@ public class ExperienceDialogFragment extends AbstractCharacterDialogFragment {
         your_xp_group = (ViewGroup) view.findViewById(R.id.your_xp_group);
         your_xp = (TextView) view.findViewById(R.id.your_xp);
 
+        xp_levels = (RecyclerView) view.findViewById(R.id.xp_levels);
+
+        xpAdapter = new XpLevelAdapter(this, getLevels());
+        xp_levels.setAdapter(xpAdapter);
+
+        xp_levels.setHasFixedSize(false);
+        xp_levels.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST);
+        xp_levels.addItemDecoration(itemDecoration);
+
+
         return view;
+    }
+
+    private List<XpLevel> getLevels() {
+        List<XpLevel> result = new ArrayList<>();
+        result.add(new XpLevel(1, 0));
+        result.add(new XpLevel(2, 300));
+        result.add(new XpLevel(3, 900));
+        result.add(new XpLevel(4, 2700));
+        result.add(new XpLevel(5, 6500));
+        result.add(new XpLevel(6, 14000));
+        result.add(new XpLevel(7, 23000));
+        result.add(new XpLevel(8, 34000));
+        result.add(new XpLevel(9, 48000));
+        result.add(new XpLevel(10, 64000));
+        result.add(new XpLevel(11, 85000));
+        result.add(new XpLevel(12, 100000));
+        result.add(new XpLevel(13, 120000));
+        result.add(new XpLevel(14, 140000));
+        result.add(new XpLevel(15, 165000));
+        result.add(new XpLevel(16, 195000));
+        result.add(new XpLevel(17, 225000));
+        result.add(new XpLevel(18, 265000));
+        result.add(new XpLevel(19, 305000));
+        result.add(new XpLevel(20, 355000));
+        return result;
     }
 
     @Override
@@ -65,7 +113,7 @@ public class ExperienceDialogFragment extends AbstractCharacterDialogFragment {
     }
 
     protected void updateViews() {
-        start_xp.setText(getCharacter().getXp() + "");
+        start_xp.setText(String.format("%,d", getCharacter().getXp()));
 
         int partySize = Math.abs(getPartySize());
 
@@ -74,13 +122,13 @@ public class ExperienceDialogFragment extends AbstractCharacterDialogFragment {
         if (partySize != 1) {
             your_xp_group.setVisibility(View.VISIBLE);
             xp = xp / partySize;
-            your_xp.setText(xp + "");
+            your_xp.setText(String.format("%,d", xp));
         } else {
             your_xp_group.setVisibility(View.GONE);
         }
 
 
-        end_xp.setText((getCharacter().getXp() + xp) + "");
+        end_xp.setText(String.format("%,d", (getCharacter().getXp() + xp)));
 
 
         party_size.addTextChangedListener(new TextWatcher() {
@@ -162,4 +210,72 @@ public class ExperienceDialogFragment extends AbstractCharacterDialogFragment {
 
         return super.onDone();
     }
+
+    static class XpLevel {
+        int level;
+        int xp;
+
+        public XpLevel(int level, int xp) {
+            this.level = level;
+            this.xp = xp;
+        }
+    }
+
+    static class XpLevelViewHolder extends BindableComponentViewHolder<XpLevel, ExperienceDialogFragment, XpLevelAdapter> {
+        TextView xp;
+        TextView level;
+        TextView comment;
+
+        public XpLevelViewHolder(View itemView) {
+            super(itemView);
+            xp = (TextView) itemView.findViewById(R.id.xp);
+            level = (TextView) itemView.findViewById(R.id.level);
+            comment = (TextView) itemView.findViewById(R.id.comment);
+        }
+
+        @Override
+        public void bind(ExperienceDialogFragment context, XpLevelAdapter adapter, XpLevel info) {
+            xp.setText(String.format("%,d", info.xp));
+            level.setText(info.level + "");
+            int currentLevel = context.getCharacter().getCharacterLevel();
+            int currentXp = context.getCharacter().getXp();
+            comment.setText("");
+            if (currentLevel == info.level) {
+                // TODO highlight background
+                comment.setText("Current Level");
+            } else if (currentLevel == info.level - 1) {
+                int needed = info.xp - currentXp;
+                if (needed > 0) {
+                    comment.setText("Need " + String.format("%,d", needed));
+                }
+            }
+        }
+    }
+
+    static class XpLevelAdapter extends RecyclerView.Adapter<XpLevelViewHolder> {
+        List<XpLevel> list;
+        ExperienceDialogFragment context;
+
+        XpLevelAdapter(ExperienceDialogFragment context, List<XpLevel> list) {
+            this.list = list;
+            this.context = context;
+        }
+
+        @Override
+        public XpLevelViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View newView = LayoutInflater.from(parent.getContext()).inflate(R.layout.xp_level_row, parent, false);
+            return new XpLevelViewHolder(newView);
+        }
+
+        @Override
+        public void onBindViewHolder(XpLevelViewHolder holder, int position) {
+            holder.bind(context, this, list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+
 }
