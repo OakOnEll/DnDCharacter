21997b58cfd0cbe597da5bf9b8268cd97054e3ad
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-02-06 13:28:20 -0500

    Add effects and conditions

--- app/src/main/java/com/oakonell/dndcharacter/model/character/SpeedType.java
+++ app/src/main/java/com/oakonell/dndcharacter/model/character/SpeedType.java
@@ -6,7 +6,7 @@ import com.oakonell.dndcharacter.R;
  * Created by Rob on 2/5/2016.
  */
 public enum SpeedType {
-    WALK(R.string.walk), FLY(R.string.fly), SWIM(R.string.swim), BURROW(R.string.burrow), CLIMB(R.string.climb);
+    WALK(R.string.walk), FLY(R.string.fly), SWIM(R.string.swim), BURROW(R.string.burrow), CLIMB(R.string.climb), CRAWL(R.string.crawl);
 
     private final int stringResId;
 
