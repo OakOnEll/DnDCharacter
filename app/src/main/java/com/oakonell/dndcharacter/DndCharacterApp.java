025d1e9628f71f90ce48c2aaf603b937239f5fe2
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-02-10 21:54:50 -0500

    Add fabric for crash reporting

--- app/src/main/java/com/oakonell/dndcharacter/DndCharacterApp.java
+++ app/src/main/java/com/oakonell/dndcharacter/DndCharacterApp.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.activeandroid.app.Application;
+import com.crashlytics.android.Crashlytics;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Rob on 11/2/2015.
@@ -21,6 +23,7 @@ public class DndCharacterApp extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         refWatcher = LeakCanary.install(this);
     }
 }
