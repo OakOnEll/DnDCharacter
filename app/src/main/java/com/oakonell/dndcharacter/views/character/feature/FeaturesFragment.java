d2754298b87696a818ba2083c7a0fb0cd1bf30a4
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-03-25 14:01:47 -0400

    support delete and navigate through feature sheet view of adjustments

--- app/src/main/java/com/oakonell/dndcharacter/views/character/feature/FeaturesFragment.java
+++ app/src/main/java/com/oakonell/dndcharacter/views/character/feature/FeaturesFragment.java
@@ -18,11 +18,17 @@ import com.oakonell.dndcharacter.model.character.CustomAdjustmentType;
 import com.oakonell.dndcharacter.model.character.CustomAdjustments;
 import com.oakonell.dndcharacter.model.character.FeatureInfo;
 import com.oakonell.dndcharacter.model.character.feature.FeatureContextArgument;
+import com.oakonell.dndcharacter.model.character.stats.StatType;
 import com.oakonell.dndcharacter.utils.NumberUtils;
 import com.oakonell.dndcharacter.views.BindableComponentViewHolder;
 import com.oakonell.dndcharacter.views.DividerItemDecoration;
 import com.oakonell.dndcharacter.views.character.AbstractSheetFragment;
 import com.oakonell.dndcharacter.views.character.CharacterActivity;
+import com.oakonell.dndcharacter.views.character.feat.InitiativeDialogFragment;
+import com.oakonell.dndcharacter.views.character.feat.PassivePerceptionDialogFragment;
+import com.oakonell.dndcharacter.views.character.item.ArmorClassDialogFragment;
+import com.oakonell.dndcharacter.views.character.race.SpeedDialogFragment;
+import com.oakonell.dndcharacter.views.character.stats.StatBlockDialogFragment;
 
 import org.solovyev.android.views.llm.LinearLayoutManager;
 
@@ -274,20 +280,24 @@ public class FeaturesFragment extends AbstractSheetFragment {
         }
     }
 
-    private static class AdjustmentRowViewHolder extends BindableComponentViewHolder<CustomAdjustments.Adjustment, CharacterActivity, RecyclerView.Adapter<?>> {
+    private static class AdjustmentRowViewHolder extends BindableComponentViewHolder<CustomAdjustments.Adjustment, CharacterActivity, AdjustmentTypeAdapter> {
         private CheckBox enable;
         private TextView value;
         private TextView comment;
+        private View adjustment_group;
+        private View delete;
 
         public AdjustmentRowViewHolder(View view) {
             super(view);
             enable = (CheckBox) view.findViewById(R.id.enable);
             value = (TextView) view.findViewById(R.id.value);
             comment = (TextView) view.findViewById(R.id.comment);
+            adjustment_group = view.findViewById(R.id.adjustment_group);
+            delete = view.findViewById(R.id.delete);
         }
 
         @Override
-        public void bind(final CharacterActivity context, RecyclerView.Adapter<?> adapter, final CustomAdjustments.Adjustment info) {
+        public void bind(final CharacterActivity context, final AdjustmentTypeAdapter adapter, final CustomAdjustments.Adjustment info) {
             value.setText(NumberUtils.formatNumber(info.numValue));
             comment.setText(info.comment);
             enable.setOnCheckedChangeListener(null);
@@ -300,6 +310,72 @@ public class FeaturesFragment extends AbstractSheetFragment {
                     context.saveCharacter();
                 }
             });
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapter.adjustments.delete(info);
+                    // TODO support undo..
+                    context.updateViews();
+                    context.saveCharacter();
+                }
+            });
+            adjustment_group.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (adapter.adjustments.getType()) {
+
+                        case MODIFYING_ACS:
+                        case ROOT_ACS:
+                            ArmorClassDialogFragment acDialog = ArmorClassDialogFragment.createDialog();
+                            acDialog.show(adapter.context.getSupportFragmentManager(), "ac");
+                            break;
+
+                        case PASSIVE_PERCEPTION:
+                            PassivePerceptionDialogFragment percDialog = PassivePerceptionDialogFragment.create();
+                            percDialog.show(adapter.context.getSupportFragmentManager(), "passivePerception");
+                            break;
+                        case INITIATIVE:
+                            InitiativeDialogFragment initDialog = InitiativeDialogFragment.create();
+                            initDialog.show(adapter.context.getSupportFragmentManager(), "initiative");
+                            break;
+
+                        case SPEED_BURROW:
+                        case SPEED_CLIMB:
+                        case SPEED_CRAWL:
+                        case SPEED_FLY:
+                        case SPEED_SWIM:
+                        case SPEED_WALK:
+                            SpeedDialogFragment speedDialog = SpeedDialogFragment.create();
+                            speedDialog.show(adapter.context.getSupportFragmentManager(), "speed");
+                            break;
+
+                        case STAT_STRENGTH:
+                            launchStatDialog(adapter, StatType.STRENGTH);
+                            break;
+                        case STAT_DEXTERITY:
+                            launchStatDialog(adapter, StatType.DEXTERITY);
+                            break;
+                        case STAT_CONSTITUTION:
+                            launchStatDialog(adapter, StatType.CONSTITUTION);
+                            break;
+
+                        case STAT_CHARISMA:
+                            launchStatDialog(adapter, StatType.CHARISMA);
+                            break;
+                        case STAT_INTELLIGENCE:
+                            launchStatDialog(adapter, StatType.INTELLIGENCE);
+                            break;
+                        case STAT_WISDOM:
+                            launchStatDialog(adapter, StatType.WISDOM);
+                            break;
+                    }
+                }
+            });
+        }
+
+        protected void launchStatDialog(AdjustmentTypeAdapter adapter, StatType statType) {
+            StatBlockDialogFragment statDialog = StatBlockDialogFragment.create(adapter.context.getCharacter().getStatBlock(statType));
+            statDialog.show(adapter.context.getSupportFragmentManager(), statType.toString());
         }
     }
 
