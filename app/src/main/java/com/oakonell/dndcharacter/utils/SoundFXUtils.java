fbc5c26ad8295e1343d456a2e338f03a816066e6
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-04-20 10:35:41 -0400

    Add more dice roll mp3s, for some variety

--- app/src/main/java/com/oakonell/dndcharacter/utils/SoundFXUtils.java
+++ app/src/main/java/com/oakonell/dndcharacter/utils/SoundFXUtils.java
@@ -13,13 +13,28 @@ import com.oakonell.dndcharacter.views.settings.SettingsActivity;
  * Created by Rob on 4/19/2016.
  */
 public class SoundFXUtils {
-    
+
     public static void playDiceRoll(Context context) {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         boolean enableSoundFX = sharedPref.getBoolean(SettingsActivity.KEY_PREF_SOUND_FX, true);
         if (!enableSoundFX) return;
 
-        MediaPlayer mp = MediaPlayer.create(context, R.raw.dice_roll);
+        int randRollSound = RandomUtils.random(1, 3);
+        MediaPlayer mp;
+        switch (randRollSound) {
+            case 1:
+                mp = MediaPlayer.create(context, R.raw.dice_roll);
+                break;
+            case 2:
+                mp = MediaPlayer.create(context, R.raw.dice_roll2);
+                break;
+            case 3:
+                mp = MediaPlayer.create(context, R.raw.dice_roll3);
+                break;
+            default:
+                mp = MediaPlayer.create(context, R.raw.dice_roll);
+                break;
+        }
         mp.start();
     }
 
