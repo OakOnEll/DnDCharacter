a3655fa2a6148c27bb76e73b267ff4e8c91a4cdb
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-11-12 07:44:50 -0500

    Add request header to data URL importer
    prevent 403 error

--- app/src/main/java/com/oakonell/dndcharacter/model/DataImporter.java
+++ app/src/main/java/com/oakonell/dndcharacter/model/DataImporter.java
@@ -98,6 +98,7 @@ public class DataImporter {
         HttpURLConnection connection = null;
         try {
             connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             connection.connect();
 
             // expect HTTP 200 OK, so we don't mistakenly save error report
