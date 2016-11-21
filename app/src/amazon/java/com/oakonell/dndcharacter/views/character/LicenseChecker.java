78d9fca8ef72eb9b38f2b833cdaf0c325632c2d3
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2016-11-21 09:46:08 -0500

    Touch up amazon "license checker"

--- app/src/amazon/java/com/oakonell/dndcharacter/views/character/LicenseChecker.java
+++ app/src/amazon/java/com/oakonell/dndcharacter/views/character/LicenseChecker.java
@@ -4,16 +4,12 @@ import android.app.Activity;
 import android.os.Handler;
 import android.provider.Settings;
 
-import com.google.android.vending.licensing.AESObfuscator;
-import com.google.android.vending.licensing.LicenseCheckerCallback;
-import com.google.android.vending.licensing.Policy;
-import com.google.android.vending.licensing.ServerManagedPolicy;
 
 /**
  * Created by Rob on 3/14/2016.
  */
 public class LicenseChecker {
-    public static final int RETRY_REASON = Policy.RETRY;
+    public static final int RETRY_REASON = -1;//Policy.RETRY;
 
     private LicenseCallback callback;
     private Activity context;
